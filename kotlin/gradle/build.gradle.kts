56146e737adc4382e8ae76f83425a87122746eab
slandelle <slandelle@gatling.io>
2025-05-13 07:44:55 +0200

    feat: upgrade Gatling 3.14.1

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   kotlin("plugin.allopen")    version "2.1.20"
 
   id("com.diffplug.spotless") version "7.0.3"
-  id("io.gatling.gradle")     version "3.14.0.2"
+  id("io.gatling.gradle")     version "3.14.1"
 }
 
 repositories {
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-  gatlingImplementation("io.gatling:gatling-mqtt-java:3.14.0")
+  gatlingImplementation("io.gatling:gatling-mqtt-java:3.14.1")
 }
 
 gatling {
