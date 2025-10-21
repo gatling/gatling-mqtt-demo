f55d3a327a0810258b4ee9308ef101dd7945a897
slandelle <slandelle@gatling.io>
2025-10-21 15:42:55 +0200

    chore: upgrade gatling-gradle-plugin 3.14.6.3

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   kotlin("plugin.allopen")    version "2.2.20"
 
   id("com.diffplug.spotless") version "8.0.0"
-  id("io.gatling.gradle")     version "3.14.6"
+  id("io.gatling.gradle")     version "3.14.6.3"
 }
 
 repositories {
