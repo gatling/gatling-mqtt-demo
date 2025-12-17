b340b3cc8b3e4b5cb8410de77d8fd98d3f5a23e6
slandelle <slandelle@gatling.io>
2025-12-17 22:44:59 +0100

    chore: upgrade kotlin 2.3.0

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   idea
-  kotlin("jvm")               version "2.2.21"
-  kotlin("plugin.allopen")    version "2.2.21"
+  kotlin("jvm")               version "2.3.0"
+  kotlin("plugin.allopen")    version "2.3.0"
 
   id("com.diffplug.spotless") version "8.1.0"
   id("io.gatling.gradle")     version "3.14.9.1"
