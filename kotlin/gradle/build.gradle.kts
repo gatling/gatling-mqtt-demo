bb9abbd1cfce0ed79b060ca7d017bf09a99552ab
slandelle <slandelle@gatling.io>
2025-12-17 23:14:51 +0100

    chore: upgrade gatling plugins

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   kotlin("plugin.allopen")    version "2.3.0"
 
   id("com.diffplug.spotless") version "8.1.0"
-  id("io.gatling.gradle")     version "3.14.9.1"
+  id("io.gatling.gradle")     version "3.14.9.2"
 }
 
 tasks.withType(JavaCompile::class) {
